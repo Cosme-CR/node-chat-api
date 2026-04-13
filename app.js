c5e02a4fafed080295215d2fd0a82c5c5635c4a1
Cosme-CR <Cosmecruztm@gmail.com>
2026-04-12 22:13:50 -0300

    so falta a ultima função e quase todas as rotas

--- app.js
+++ app.js
@@ -15,32 +15,29 @@ app.use(cors(corsOptions))
 // response retornos da API
 //request  sao chegada de dados da api
 
-const estadCidade = require("./module/funcoes.js")
+const zap = require("./module/funcao.js")
+//console.log('zap:', zap);
 
 
 //criando ENDpoint para API
 //cada endpoit retorna um json
 
 //mostra a lista de estados do brasil
-app.get("/v1/senai/dados/estados", function(request,response){
-    let estado = estadCidade.getListaDeEstado()
-    
-    //mostra q ta ok
-    response.status(200)
-    response.json(estado)
+app.get("/v1/whatsapp/tdsmsgconta/:nick", function(request,response){
+    let nick    = request.params.nick
+    let msg     = zap.listaTodasMensagen(nick)
+
+    if(msg){
+        response.status(200)
+        response.json(msg)
+    }else{
+        response.status(404)
+        response.json({"mensagem": "usuario não exite ou nao tem mensagens "})
+    }
     
 })
 
 
 
-
-
-
-
-
-
-
-
-
 //serve pra iniciar a api
-app.listen(8080,function(){console.log("api funcionando")})
+app.listen(8080,function(){console.log("api funcionando em http://localhost:8080")})
--- module/funcao.js
+++ module/funcao.js
@@ -14,29 +14,144 @@ function getListaTdsDados(){
 
 }
 
-console.log(getListaTdsDados())
+//:whats-usersconsole.log(getListaTdsDados())
 //console.log(objLista.contatos)
 
 
 
 
+//função que faz a busca pelo nick name e retorna os dados no json
+function getDadosContaUsuario(nick){
+
+    let retorno = {}
+    let statu = false
+
+    for(let dado of objLista.contatos["whats-users"]){
+        if(nick.toUpperCase() == dado.nickname.toUpperCase()){
+            retorno.nome            = dado.account
+            retorno.nickname        = dado.nickname
+            retorno.criacaoConta    = dado["created-since"]
+            retorno.foto            = dado["profile-image"]
+            retorno.numero          = dado.number
+            retorno.background      = dado.background
+            
+            statu = true
+        }
+        
+    }
+
+    if(statu){
+        return retorno
+    }else{
+        return false
+    }
+
+}
+
+//console.log(getDadosContaUsuario("Sand"))
 
 
+//função pra retornar todos os dados de contato de um usuario passando o nick do usuario 
+function getDadosDeContatosDoUsuario(nick){  
+    //objeto de retorno
+    let retorno = {}
+    //vetor que vai receber a lista de contato
+    let contato = []
+    let statu   = true
+
+    for(let usuario of objLista.contatos["whats-users"]){
+        if(usuario.nickname.toUpperCase() == nick.toUpperCase()){
+            retorno.nome        = usuario.account
+            retorno.numero      = usuario.number
+            //retorno = contatos    = usuario.contacts
+            //percorrer os contatos 
+            for(let ctt of usuario.contacts){
+                contato.push({
+                    nome        : ctt.name, 
+                    descricao   : ctt.description,
+                    foto        : ctt.image
+                })
+            }
+            retorno.contatos    = contato
+
+            statu = true
+        }
+    }
+
+    if(statu){
+        return retorno
+    }else{return false}
+
+}
+
+//console.log(getDadosDeContatosDoUsuario("Sand"))
+
+function listaTodasMensagen(nick){
+
+    let retorno = {}
+    let statu   = false
+
+    let contat=[]
+    
+    for(let usuario of objLista.contatos["whats-users"]){
+        if(nick.toUpperCase() == usuario.nickname.toUpperCase()){
+            retorno.nome = usuario.account
+
+            for(let ctt of usuario.contacts){
+               
+                contat.push({
+                    nome:       ctt.name,
+                    descricao:  ctt.description,
+                    foto:       ctt.image,
+ 
+                    mensagem:   ctt.messages
+
+                })
+            }
+            retorno.contatos = contat
+            //retorno.contatos = usuario.contacts
+            statu = true
+        }
+    }
+
+    if(statu){
+        return retorno
+    }else{return false}
+
+}
+//console.log(listaTodasMensagen("Sand"))
+
+function listaMsgCttUsuario(nick,ctt){
+    let retorno = {}
+    let statu    = false
+    for(let usuario of objLista.contatos["whats-users"]){
+        if(nick.toUpperCase() == usuario.nickname.toUpperCase()){
+            for(let contat of usuario.contacts){
+                if(ctt.toUpperCase() == contat.name.toUpperCase()){
+                    retorno.usuario     = nick
+                    retorno.numero      = usuario.number 
+                    
+
+                    retorno.nome        = contat.name
+                    retorno.descricao   = contat.description
+                    retorno.mensagens   = contat.messages
+
+                    statu = true
+                }
+            }
+        }
+    }
+
+    if(statu){
+        return retorno
+    }else{return false}
+}
 
 
-
-
-
-
-
-
-
-
-
-
+console.log(listaMsgCttUsuario("Ricky","Ana Maria"))
 
 module.exports={
 
  
-    
+   listaTodasMensagen, 
 }
